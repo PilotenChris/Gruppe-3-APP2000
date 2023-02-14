6100e85e29106e274b4bff3e1153ff884e145003
PilotenChris <45621350+PilotenChris@users.noreply.github.com>
2023-02-14 17:56:04 +0100

    Fixed file structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+import React from 'react';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Layout from "./Layout.js"
+import Home from "./Home.js"
+import Unkownpage from "./Unkownpage.js"
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/*" element={<Unkownpage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
