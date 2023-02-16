d707ad96fe5ccd4ff5df3adacfc6f24e59d0f27d
Omar Abdulle <93823797+siroaa7@users.noreply.github.com>
2023-02-16 14:11:24 +0100

    Sidebar with dropdown menu

---
+++ frontend/src/Home.js
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import Sidenav from "./Sidenav";
+
+const Home = () => {
+    return (
+        <div>
+          <Sidenav>
+          </Sidenav>
+        </div>
+      );
+
+
+};
+
+export default Home;
---
+++ frontend/src/Sidenav.js
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import "./Sidenav.css";
+
+const Sidenav = (props) => {
+  const [isSidenavOpen, setIsSidenavOpen] = useState(false);
+
+  const openSidenav = () => {
+    setIsSidenavOpen(true);
+  };
+
+  const closeSidenav = () => {
+    setIsSidenavOpen(false);
+  };
+
+  return (
+    <>
+      <div
+        id="mySidenav"
+        className="sidenav"
+        style={{ width: isSidenavOpen ? "250px" : "0" }}
+      >
+        <a href="javascript:void(0)" className="closebtn" onClick={closeSidenav}>
+          &times;
+        </a>
+        <div className="menu-container">
+          <select className="menu-dropdown">
+            <option value="Firma">Firma</option>
+          </select>
+        </div>
+        <div className="menu-container">
+          <select className="menu-dropdown">
+            <option value="Bil">Bil</option>
+          </select>
+        </div>
+        <div className="menu-container">
+          <select className="menu-dropdown">
+            <option value="Batteri">Batteri</option>
+          </select>
+        </div>
+      </div>
+
+      <div style={{ height: "100%", width: "100%" }}>
+        <span
+          style={{ fontSize: "30px", cursor: "pointer" }}
+          onClick={openSidenav}
+        >
+          &#9776;
+        </span>
+        <div className="App" style={{ height: "100%", width: "100%" }}>
+          {props.children}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Sidenav;
