bf09b79a1bf3db84394e2bd32eb3369cfa697e12
PilotenChris <45621350+PilotenChris@users.noreply.github.com>
2023-02-17 04:33:47 +0100

    Fixed some mistakes in the Sidenav and removed Layout as it created problems

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import {BrowserRouter, Routes, Route} from "react-router-dom";
-import Layout from "./Layout.js"
 import Home from "./Home.js"
 import Unkownpage from "./Unkownpage.js"
 import './App.css';
@@ -9,7 +8,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout />}>
+        <Route path="/">
           <Route index element={<Home />} />
           <Route path="/*" element={<Unkownpage />} />
         </Route>
--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,4 +1,3 @@
-import React, { useState } from "react";
 import Sidenav from "./Sidenav";
 
 const Home = () => {
