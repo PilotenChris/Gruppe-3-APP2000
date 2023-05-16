84144cabb9add3395387a3427330fb75e575a4b7
Omar Abdulle <93823797+siroaa7@users.noreply.github.com>
2023-05-16 13:26:10 +0200

    removed some files

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,11 +1,8 @@
 import Sidenav from "./Sidenav";
 import Map from "./Map";
-import Admin from "./Admin";
-
 const Home = () => {
     return (
         <div><Map></Map>
-        <Admin></Admin>
           <Sidenav></Sidenav>
         </div>
       );
