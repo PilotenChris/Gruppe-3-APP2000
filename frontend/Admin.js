7c5f0dd7875506dc92bdf8e1fb787b741d42c2fb
Omar Abdulle <93823797+siroaa7@users.noreply.github.com>
2023-05-16 13:22:40 +0200

    Added Admin files

---
+++ frontend/Admin.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from "react";
+import "./Admin.css";
+
+const Admin = (props) => {
+ 
+
+        return (
+            <div>
+              <button type="button">Click Me</button>
+            </div>
+          );
+    
+}
+
+export default Admin;
--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,9 +1,11 @@
 import Sidenav from "./Sidenav";
 import Map from "./Map";
+import Admin from "./Admin";
 
 const Home = () => {
     return (
         <div><Map></Map>
+        <Admin></Admin>
           <Sidenav></Sidenav>
         </div>
       );
