af0b9a4806f37bc0fe5d3c9892fdb5807d287379
Omar Abdulle <93823797+siroaa7@users.noreply.github.com>
2023-05-16 14:24:31 +0200

    Added Admin button

---
+++ frontend/src/Admin.js
@@ -0,0 +1,15 @@
+import "./Admin.css"
+const Admin = () => {
+    const handleAdminClick = () => {
+        
+    }
+
+    return (
+        <div className="admin-button">
+            <button onClick={handleAdminClick}>Admin</button>
+        </div>
+
+    )
+}
+
+export default Admin
--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,13 +1,14 @@
 import Sidenav from "./Sidenav";
 import Map from "./Map";
+import Admin from "./Admin";
+
 const Home = () => {
     return (
         <div><Map></Map>
           <Sidenav></Sidenav>
+          <Admin></Admin>
         </div>
       );
-
-
 };
 
 export default Home;
